387ed6be1c10c37e06221cffa1d19bcd955eda27
MA-habibullah <maulana.ahmadhabibullah.22@gmail.com]>
2026-07-05 16:57:53 +0700

    feat: compile static tailwind css and remove cdn for legacy support

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.php",
+    "./app/**/*.php",
+    "./public/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
