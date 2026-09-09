5445ed6cf313ad21d2d1d12aa5c8ddde9d913177
MA-habibullah <maulana.ahmadhabibullah.22@gmail.com]>
2026-09-09 07:08:23 +0700

    style(layout): add custom favicon icon to public assets and app blade template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title inertia>{{ config('app.name', 'SINTA SaaS') }}</title>
 
+    <!-- Favicon Tab Icon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+
     <!-- Google Fonts Inter -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
